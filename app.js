6ecc569bb9fbf6cd5628b97fddbd029dcdc77908
Hongshan <zhou.hongshan636@gmail.com>
2021-08-31 17:54:49 +0200

    bind

--- app.js
+++ app.js
@@ -75,4 +75,9 @@ app.use("/api", authRouter);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 module.exports = app;
