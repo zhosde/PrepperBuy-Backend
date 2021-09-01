4906b106b6db0ce173097c0564d36033b19344e1
Hongshan <zhou.hongshan636@gmail.com>
2021-09-02 00:25:57 +0200

    test cookies

--- app.js
+++ app.js
@@ -41,10 +41,10 @@ app.use(
     secret: process.env.SESSION_SECRET || "very secret stuff",
     resave: true,
     saveUninitialized: false,
-    cookie: {
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // must be 'none' to enable cross-site delivery
-      secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
-    },
+    // cookie: {
+    //   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // must be 'none' to enable cross-site delivery
+    //   secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
+    // },
   })
 );
 
